98a134002d4c4d3a7766100327b4a3249a2b2c46
Mohammed Alrashdi <fgtre9990@gmail.com>
2026-06-05 22:53:44 +0300

    calualte pts and hours and add them to final gpa and show the output for user's courses (finished)

--- CalGPA.cpp
+++ CalGPA.cpp
@@ -8,8 +8,10 @@ using namespace std;
 struct Sub
  {
     string name;
-    int hours;
-    int grade;
+    int hours=0;
+    int grade=0;
+    float pt=0;
+    string letter;
  };
 
 int main(){
@@ -19,7 +21,7 @@ int main(){
   
 Sub H[size];
 
-  cout<< "enter the total hours :"<<endl;
+  cout<< "enter the total hours :"<<endl; //حساب المعدل الحالي
   cin>>Thours;
 
   cout<<"enter the total points :"<<endl;
@@ -40,7 +42,7 @@ Sub H[size];
  
 } while (N>10 || N<=0);
  
- for(int i=0;i<N;i++){
+ for(int i=0;i<N;i++){ //طلب المواد الي راح يسجله حق الترم الجاي
  
 
 
@@ -48,14 +50,116 @@ Sub H[size];
  cin.ignore();
  getline(cin,H[i].name);
 
- cout<<"how many hours :\n";
+ cout<<"how many hours(1-4) :\n";
  cin>>H[i].hours;
 
- cout<<"grade:\n";
+ cout<<"grade(from 0-100 only):\n";
  cin>>H[i].grade;
 
 
  }
 
+ for (int i = 0; i < N; i++)// نعطي النقاط لكل مادة حسب القريد
+ {
+
+   if (H[i].grade>=95)
+   {
+      H[i].pt = 5.00 * H[i].hours;
+      H[i].letter ="A+";
+
+   }
+  
+
+   else if (H[i].grade>=90)
+   {
+      H[i].pt = 4.75 * H[i].hours;
+      H[i].letter = 'A';
+
+   }
+
+   else if (H[i].grade>=85)
+   {
+      H[i].pt = 4.50 * H[i].hours;
+      H[i].letter = "B+";
+
+   }
+   else if (H[i].grade>=80)
+   {
+      H[i].pt = 4.00 * H[i].hours;
+      H[i].letter = 'B';
+
+   }
+
+   else if (H[i].grade>=75)
+   {
+      H[i].pt = 3.50 * H[i].hours;
+      H[i].letter = "C+";
+
+   }
+
+   else if (H[i].grade>=70)
+   {
+      H[i].pt = 3.00 * H[i].hours;
+      H[i].letter = 'C';
+
+   }
+   else if (H[i].grade>=65)
+   {
+      H[i].pt = 2.50 * H[i].hours;
+      H[i].letter = "D+";
+
+   }
+
+   else if (H[i].grade>=60)
+   {
+      H[i].pt = 2.00 * H[i].hours;
+      H[i].letter = 'D';
+
+   }
+
+   else if (H[i].grade<=60)
+   {
+      H[i].pt = 1.00 * H[i].hours;
+      H[i].letter = 'F';
+
+   }
+
+ }
+float fpoint =0;
+int fhour =0;
+  for (int i = 0; i < N; i++) //حساب النقاط والساعات للترم الجاي
+  {
+    fpoint+= H[i].pt;
+    //cout<<"points: "<<fpoint<<endl; 
+    fhour+= H[i].hours;
+      //cout<<"hours: "<<fhour<<endl;
+
+  }
+  //cout<<fpoint<<endl<<
+  //fhour<<endl;
+  float tgpa = fpoint / fhour;
+  tgpa = floor(tgpa *100) /100; //حساب المعدل الفصلي
+//  cout<<"your class gpa is : "<<tgpa; 
+  
+   float Npoint = fpoint + tpoints;//حساب المعدل الترم الجاي
+   int Nhour = fhour + Thours;
+   float Ngpa= Npoint/Nhour;
+   Ngpa = floor(Ngpa *100) / 100;
+
+
+   for(int i=0;i<N;i++){
+
+      cout<<"---------------------------------------------"<<endl;
+      cout<<"course Name: "<<H[i].name<<endl;
+      cout<<"hours: "<<H[i].hours<<endl;
+      cout<<"Grade: "<<H[i].letter<<endl;
+      cout<<"---------------------------------------------"<<endl;
+
+   }
+
+
+   cout<<"المعدل الحالي : "<<fgpa<<endl;
+   cout<<"المعدل الفصلي: "<<tgpa<<endl;
+   cout<<"المعدل التراكمي (المتوقع): "<<Ngpa<<endl;
     return 0;
 }
